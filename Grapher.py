62f8771c85d3dd7f0020d4cb5225fd648ab99a9a
aziznal <abodenaal@gmail.com>
2020-09-02 23:22:57 +0300

    Change the way tick labels are shown

--- Grapher.py
+++ Grapher.py
@@ -4,9 +4,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Bank Line Colors
+ISBANK = '#0042ff'          # Blue
+KUVEYTTURK = '#00ff78'      # Green
+VAKIF = '#e48a00'           # Yellow
+YAPIKREDI = '#00d8ff'       # Light Blue
+ZIRAAT = '#ff2a00'          # Red
+
+
+def get_color_from_bankname(bankname):
+
+    bankname = bankname.lower()
+
+    print(bankname)
+
+    if bankname == 'i̇şbankası':
+        return ISBANK
+
+    if bankname == 'küveyttürk':
+        return KUVEYTTURK
+
+    if bankname == 'vakıf':
+        return VAKIF
+
+    if bankname == 'yapıkredi':
+        return YAPIKREDI
+
+    if bankname == 'ziraat':
+        return ZIRAAT
+
+
 class Grapher:
     def __init__(self, data: pd.DataFrame, intervals=None, bankname="", save_graph=False, graph_filename=""):
-        
+
         self.bankname = bankname
 
         self._save_graph = save_graph
@@ -27,7 +57,7 @@ class Grapher:
             if index == 0:
                 prev_val = row
                 continue
-            
+
             # Actual Loop
             if prev_val[0] == row[0]:
                 duplicate_counter += 1
@@ -45,13 +75,13 @@ class Grapher:
             hour += ":30"
         else:
             hour += ":00"
-        
+
         fixed_datetime = "_".join([date, hour])
 
         return fixed_datetime
 
     def _fix_duplicates(self, data):
-        
+
         prev_val = None
 
         for index, row in data.iterrows():
@@ -62,7 +92,7 @@ class Grapher:
                 new_val = self._create_new_time_val(row[0], True)
                 data.at[index, 'time'] = new_val
                 continue
-            
+
             # Actual Loop
             if prev_val[0] == row[0]:
                 new_val = self._create_new_time_val(row[0])
@@ -115,7 +145,7 @@ class Grapher:
         self._repair_time_column(data)
 
         self._create_date_column(data)
-        
+
         self._create_hour_column(data)
 
         return data
@@ -124,7 +154,7 @@ class Grapher:
         """
         return index of first row with 'date' and 'hour_minute' columns
         matching given value. Raise Exception otherwise.
-        """    
+        """
         date, time = value.split(' ')
 
         time_val = ":".join(time.split(':')[:2]) + ":00"
@@ -135,11 +165,10 @@ class Grapher:
             val = row[-1]
 
             if val == time_val:
-                print(f"Found matching val at {i}")
                 return i
 
         return -1
-            
+
     def _apply_intervals(self, intervals):
         """
         sets data to only the rows that are within the given intervals
@@ -164,21 +193,39 @@ class Grapher:
         else:
             self._apply_intervals(intervals)
 
+    def get_tick_labels(self, tick_positions):
+        for i in tick_positions:
+            yield self.data.at[i, 'date'] + " " + self.data.at[i, 'hour_minute']
+
+    def set_xtick_labels(self):
+
+        tick_pos = [i * len(self.data)//10 for i in range(10)]
+        tick_labels = list(self.get_tick_labels(tick_pos))
+
+        # Set text labels and properties.
+        plt.xticks(tick_pos, tick_labels, rotation=45, ha='right', fontsize=7)
+
     def create_graph(self):
 
-        print(self.data)
+        print(self.data.head())
 
         figure = plt.figure()
 
-        plt.plot('time', 'selling', data=self.data)
-        plt.plot('time', 'buying', data=self.data)
+        color = get_color_from_bankname(self.bankname)
+
+        plt.plot('time', 'buying', data=self.data, c=color, linewidth=1)
+        plt.plot('time', 'selling', data=self.data, c=color, linewidth=1)
 
         plt.title('USD to TL conversion rates - %s' % self.bankname)
         plt.xlabel('Time')
         plt.ylabel('Price - USD to TL')
 
         # Clear xtick labels
-        plt.xticks([])
+        # plt.xticks([])
+
+        self.set_xtick_labels()
+
+        figure.tight_layout()
 
         if self._save_graph:
             # TODO: automate filename creation
